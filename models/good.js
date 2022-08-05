9024871578517d5689b878bde218db91f71fa579
qhtkdrma <kimwonkyu15@gmail.com>
2022-08-05 11:09:08 +0900

    다음 경매 업데이트를 위한 DB 업데이트

--- models/good.js
+++ models/good.js
@@ -8,25 +8,30 @@ module.exports = class Good extends Sequelize.Model {
       name: {
         type: Sequelize.STRING(40),
         allowNull: false,
+        comment: "굿즈 이름",
       },
       img: {
         type: Sequelize.STRING(200),
         allowNull: true,
+        comment: "굿즈 이미지 파일 이름",
       },
       start: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.SMALLINT.UNSIGNED,
         allowNull: false,
         defaultValue: 1,
+        comment: "경매 준비 시간",
       },
       end: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.SMALLINT.UNSIGNED,
         allowNull: false,
         defaultValue: 24,
+        comment: "경매 종료 시간",
       },
       price: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.BIGINT.UNSIGNED,
         allowNull: false,
         defaultValue: 0,
+        comment: "경매 최초 가격",
       },
     }, {
       sequelize,
